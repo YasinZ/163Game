2ae437ef9fd008bbf6232b144cf582020ed11be4
YasinZ <yasinzuh@gmail.com>
2017-12-04 21:07:40 -0800

    Added first files

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = new Vector3(ball.transform.position.x, ball.transform.position.y + 1, ball.transform.position.z - 1);
+        transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿// iOS gyroscope example
+//
+// Create a cube with camera vector names on the faces.
+// Allow the iPhone to show named faces as it is oriented.
+
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class Movement : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        gyroEnabled = EnableGyro();
+        Debug.Log(gyroEnabled);
+    }
+
+    private bool EnableGyro()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Update()
+    {
+        if(gyroEnabled)
+        {
+            Debug.Log(gyro.rotationRate.y);
+            if(gyro.rotationRate.y > 0)
+                GetComponent<Rigidbody>().AddForce(Vector3.right * 20);
+            else
+                GetComponent<Rigidbody>().AddForce(Vector3.left * 20);
+
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * 10);
+        }
+    }
+}
